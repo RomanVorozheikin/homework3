fdef004f988a57267daa32e39f2df429778a0da9
roma <rommur5@mail.ru>
2023-04-05 17:58:48 +0300

    задача1

--- src/java/ru/roman/Main.java
+++ src/java/ru/roman/Main.java
@@ -2,6 +2,17 @@ package ru.roman;
 
 public class Main {
     public static void main(String[] args) {
-
+        int cat=1500000;
+        byte dog=100;
+        short paper=29990;
+        long bird=350000000;
+        float goose=10.557585867F;
+        double rabbit=555.679028193;
+        System.out.println("Значение переменной cat с типом int равно "+cat);
+        System.out.println("Значение переменной dog с типом byte равно "+dog);
+        System.out.println("Значение переменной paper с типом short равно "+paper);
+        System.out.println("Значение переменной bird с типом long равно "+bird);
+        System.out.println("Значение переменной goose с типом float равно "+goose);
+        System.out.println("Значение переменной rabbit с типом double равно "+rabbit);
     }
 }
